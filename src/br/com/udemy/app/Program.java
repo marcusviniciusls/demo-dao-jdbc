a76077427ea4f7b4faf1f154fa8d39e366c7a6d4
marcusviniciusls <viniciusmls@outlook.com>
2021-06-21 22:56:09 -0300

    Seller entity class

--- src/br/com/udemy/app/Program.java
+++ src/br/com/udemy/app/Program.java
@@ -1,14 +1,19 @@
 package br.com.udemy.app;
 
+import java.util.Date;
+
 import br.com.udemy.model.entities.Department;
+import br.com.udemy.model.entities.Seller;
 
 public class Program {
 	
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1,"Books");
+		Seller s1 = new Seller(1,"Marcus Vinicius","viniciusmls@outlook.com",new Date(),3000.0,obj);
 		
 		System.out.println(obj);
+		System.out.println(s1);
 	}
 
 }
