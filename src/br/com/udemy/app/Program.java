e8cbcd1d1ebc569b08fe5649482ed021ead32438
marcusviniciusls <viniciusmls@outlook.com>
2021-06-21 22:48:54 -0300

    Department entity class

---
+++ src/br/com/udemy/app/Program.java
@@ -0,0 +1,14 @@
+package br.com.udemy.app;
+
+import br.com.udemy.model.entities.Department;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1,"Books");
+		
+		System.out.println(obj);
+	}
+
+}
